cb0eacc74d9a933830995d09565b1edc83932384
biyaG <Biyagirma@gmail.com>
2025-12-16 15:47:53 +0100

    Initial commit

---
+++ src/main/java/it/unipi/booknetapi/BookNetApiApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.booknetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unipi/booknetapi/BookNetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.booknetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
